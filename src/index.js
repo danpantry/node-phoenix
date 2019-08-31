20625f233ae906719a6d65136cef356c734787ac
Dan Pantry <devnull@danpantry.me>
2019-08-31 15:25:32 +0100

    facilitate websocket connections

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import serveStatic from "serve-static";
 import path from "path";
+import expressWs from "express-ws";
 
 const app = express();
+expressWs(app);
+
 app.set("views", path.join(process.cwd(), "views"));
 app.set("view engine", "twig");
 
@@ -14,5 +17,17 @@ app.use("/public", serveStatic(publicPath, opts));
 app.get("/", (req, res) => {
   res.render("home", { name: "World" });
 });
+
+function phoenixLiveSockets() {
+  return (ws, req) => {
+    const version = req.query.param;
+    if (version === "2.0.0") {
+      console.warn("version mismatch");
+      ws.close();
+    }
+  };
+}
+
+app.ws("/live/websocket", phoenixLiveSockets());
 
 app.listen(8080);
