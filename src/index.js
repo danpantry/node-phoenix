66407ea3c27e40132e41530b94863ce6ee281ef9
Dan Pantry <devnull@danpantry.me>
2019-08-31 14:53:45 +0100

    serve files from public/ folder next to cwd()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import serveStatic from "serve-static";
+import path from "path";
 
 const app = express();
+
+const opts = { index: false };
+const publicPath = path.join(process.cwd(), "public");
+
+app.use("/public", serveStatic(publicPath, opts));
 
 app.get("/", (req, res) => {
   res.write("Hello, world!");
